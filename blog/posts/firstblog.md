f1ea9540cb271055e8617be042291ce2a35cbe18
AbhijitL <labhijitsingh13@gmail.com>
2021-11-12 19:25:42 +0530

    Added Error 404 page.

--- blog/posts/firstblog.md
+++ blog/posts/firstblog.md
@@ -13,7 +13,7 @@ function hello(){
 }
 ```
 
-```c++
+```cpp
 #include<iostream>
 int main(){
     std::cout<<"Hello!!"<<std::endl;
---
+++ src/pages/404.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    <div class="section" id="section1">
+      <h1>Error 404</h1>
+      <p>Error 404 no page found for the specific url.</p>
+    </div>
+  </Layout>
+</template>
+
+
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+}
+
+#section1 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+</style>
