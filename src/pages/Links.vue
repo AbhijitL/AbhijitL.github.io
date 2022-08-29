867eba69b7e0daa05c2c127703d0f5f73af0f420
AbhijitL <labhijitsingh13@gmail.com>
2022-08-29 18:44:30 +0530

    change contact to links and add social links compt

---
+++ src/pages/Links.vue
@@ -0,0 +1,41 @@
+<template>
+  <Layout>
+    <div class="container skinny-contain">
+      <div class="row">
+        <div class="col">
+          <div class="breadcrumb">
+            <div class="breadcrumb-item">
+              <a title="Link back to home" href="/">Home</a>
+            </div>
+            <div class="breadcrumb-item">{{ $metaInfo.title }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid">
+        <div class="col-lg-12 gx-3">
+          <div class="row">
+            <h3>Contact me @ labhijitsingh13@gmail.com</h3>
+          </div>
+          <div class="row">
+            <p>Contact me @ labhijitsingh13@gmail.com</p>
+          </div>
+          <social-links />
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import SocialLinks from "@/components/SocialLinks.vue";
+export default {
+  metaInfo() {
+    return {
+      title: "Contact",
+    };
+  },
+  components: {
+    SocialLinks,
+  },
+};
+</script>
