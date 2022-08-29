dd2df082cfc371dcee718eb26f3501b41767446d
AbhijitL <labhijitsingh13@gmail.com>
2022-08-29 18:43:51 +0530

    added a social links component

---
+++ src/components/SocialLinks.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <a
+        href="https://www.instagram.com/labhijits/"
+        target="_blank"
+        title="Instagram"
+      >
+        <a class="fa-brands fa-instagram fa-2x"></a>
+      </a>
+    </div>
+    <div class="col">
+      <a
+        href="https://www.artstation.com/labhijits"
+        target="_blank"
+        title="Artstation"
+      >
+        <a class="fa-brands fa-artstation fa-2x"></a>
+      </a>
+    </div>
+    <div class="col">
+      <a
+        href="https://twitter.com/leihaorambam"
+        target="_blank"
+        title="Twitter"
+      >
+        <a class="fa-brands fa-twitter fa-2x"></a>
+      </a>
+    </div>
+    <div class="col">
+      <a href="https://github.com/AbhijitL" target="_blank" title="Github">
+        <a class="fa-brands fa-github fa-2x"></a>
+      </a>
+    </div>
+    <div class="col">
+      <a
+        href="https://www.youtube.com/channel/UC3MOEDdCSSyfCvmUrtln0Cg"
+        target="_blank"
+        title="Youtube"
+      >
+        <a class="fa-brands fa-youtube fa-2x"></a>
+      </a>
+    </div>
+    <div class="col">
+      <a
+        href="https://myanimelist.net/profile/ggloll"
+        target="_blank"
+        title="MyAnimeList"
+      >
+        <a class="fa-brands fa-maxcdn fa-2x"></a>
+      </a>
+    </div>
+  </div>
+</template>
