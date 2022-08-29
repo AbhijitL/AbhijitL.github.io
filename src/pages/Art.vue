b7a05004a2a5acab6f16cdcf09ddc889bd027a4b
AbhijitL <labhijitsingh13@gmail.com>
2022-08-29 22:45:03 +0530

    added gallery view

--- src/pages/Art.vue
+++ src/pages/Art.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <div class="container skinny-contain">
+    <div class="container slim-contain">
       <div class="row">
         <div class="col">
           <div class="breadcrumb">
@@ -11,21 +11,67 @@
           </div>
         </div>
       </div>
-      <div class="container-fluid"></div>
+      <p>Work in progress gallery</p>
+      <div class="fefd">
+        <gallery
+          :images="images"
+          :index="index"
+          @close="index = null"
+        ></gallery>
+        <div
+          class="image"
+          v-for="(image, imageIndex) in images"
+          :key="imageIndex"
+          @click="index = imageIndex"
+          :style="{
+            backgroundImage: 'url(' + image + ')',
+            width: '300px',
+            height: '200px',
+          }"
+        ></div>
+      </div>
     </div>
   </Layout>
 </template>
 
 <script>
+// import VueGallery from "vue-gallery";
 export default {
   metaInfo() {
     return {
       title: "Art",
     };
   },
-  components: {},
+  data: function () {
+    return {
+      images: [
+        "https://i.imgur.com/8VMtwWE.jpg",
+        "https://i.imgur.com/X5GNa4b.png",
+        "https://i.imgur.com/rw6h68U.jpg",
+        "https://i.imgur.com/OnHX0NZ.jpg",
+        "https://i.imgur.com/mKZGT9L.png",
+        "https://i.imgur.com/wkTrYxM.png",
+        "https://i.imgur.com/bER6uny.jpg",
+        "https://i.imgur.com/rm4SjfI.jpg",
+        "https://i.imgur.com/XmBrhOO.png",
+        "https://i.imgur.com/MPS5azL.png",
+      ],
+      index: null,
+    };
+  },
+
+  // components: {
+  //   gallery: VueGallery,
+  // },
 };
 </script>
-
-<style>
+<style scoped>
+.image {
+  float: left;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  border: 1px solid #ebebeb;
+  margin: 5px;
+}
 </style>
