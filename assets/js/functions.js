787c6169fe3bb4e2969175bc0eaeee1c2e441d67
Abhijit Leihaorambam <labhijitsingh13@gmail.com>
2024-06-06 18:22:59 +0530

    art gallery

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,10 @@
 // Events
 $(document).ready(make_chart_from_data);
 
+jQuery(window).on( 'load', function(){
+    window.setTimeout( setup_art_gallery_layout(), 1500 );
+});
+
 $(document).on('click', '#btnHomePage', function(){
     if($('#id-home').length > 0){
         return;
@@ -12,6 +16,21 @@ $(document).on('click', '#btnAboutPage', function(){
     if($('#id-about').length > 0){
         return;
     }
+    document.location.href='/about/';
+});
+
+$(document).on('click', '#btnArtPage', function(){
+    if($('#id-art').length > 0){
+        return;
+    }
+    document.location.href='/art/';
+});
+
+$(document).on('click', '#btnResume', function(){
+    alert("you fool!");
+});
+
+$(document).on('click', '#btnGoMore', function(){
     document.location.href='/about/';
 });
 
@@ -59,4 +78,46 @@ async function make_chart_from_data(){
     }
     var last_updated_div = `<h3>Last update: ${data["created"]} </h3>`; 
     $('.last-updated').append(last_updated_div);
+}
+
+
+function setup_art_gallery_layout(){
+    var $masonry_gallery = jQuery('.masonry-gallery.gallery');
+    if($masonry_gallery.length <= 0){
+        return;
+    }
+    console.log("Setting up ")
+    $masonry_gallery.each( function(index, element) {
+				var $masonry_items = $(element).find('.gallery-item');
+			
+				// set masonry layout
+				$(element).isotope({
+					masonry: { columnWidth: $(element).find('.gallery-item')[0] },
+					itemSelector: '.gallery-item'
+				});
+				$(element).isotope('layout');
+					
+				// filtering
+				jQuery('#gallery-filter li a').on('click', function(){
+					jQuery('#gallery-filter li a').removeClass('active');
+					jQuery(this).addClass('active');
+					var selector = jQuery(this).attr('data-filter');
+					$masonry_gallery.isotope({ filter: selector });
+					return false;
+				});
+
+				// changing layout
+				jQuery('#grid-changer li a').on('click', function(){
+					jQuery('#grid-changer li a').removeClass('active');
+					jQuery(this).toggleClass('active');
+
+					$masonry_items.removeClass('col-3');
+					$masonry_items.removeClass('col-4');
+					$masonry_items.removeClass('col-5');
+					$masonry_items.toggleClass(jQuery(this).closest('li').attr('class'));
+					$masonry_gallery.isotope('layout');
+				});
+			
+			});
+
 }
